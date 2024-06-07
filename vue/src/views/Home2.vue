7972031015923aec27d94efbd4e34719b61e2e04
unknown <1248795854@qq.com>
2024-06-08 02:25:32 +0800

    homepage_design

--- vue/src/views/Home2.vue
+++ vue/src/views/Home2.vue
@@ -1,108 +1,141 @@
 <template>
-    <el-container style="height: 800px; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu router :default-openeds="['1','2','3']" :default-active="this.$router.path">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>个人信息</template>
-        <el-menu-item-group>
-          <el-menu-item index="/changepassword">修改密码</el-menu-item>
-          <el-menu-item index="/property">当前资产</el-menu-item>
-       
-        </el-menu-item-group>
-        
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>市场数据</template>
-        <el-menu-item-group>
+  <el-container style="height: 800px; border: 1px solid #eee">
+<el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+  <el-menu router :default-openeds="[]" :default-active="this.$router.path">
+   
+    <el-submenu index="1">
+      <template slot="title"><i class="el-icon-message"></i>个人信息</template>
+      <el-menu-item-group>
+        <el-menu-item index="/changepassword">修改密码</el-menu-item>
+        <el-menu-item index="/property">当前资产</el-menu-item>
+     
+      </el-menu-item-group>
+    </el-submenu>
 
-          <el-menu-item index="/today">今日大盘</el-menu-item>
-          <el-menu-item index="/stock1">上交所</el-menu-item>
-          <el-menu-item index="/stock2">深交所</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title"><i class="el-icon-menu"></i>市场数据</template>
+      <el-menu-item-group>
+        
+        <el-menu-item index="/stock0">今日大盘</el-menu-item>
+        <el-menu-item index="/stock1">上交所</el-menu-item>
+        <el-menu-item index="/stock2">深交所</el-menu-item>
 
-        </el-menu-item-group>
-        
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>量化交易</template>
-        
-          <el-menu-item index="/trade">买入卖出</el-menu-item>
-        
-        <el-submenu index="3-2">
-          <template slot="title">交易策略</template>
-          <el-menu-item index="/strategy1">策略1</el-menu-item>
-          <el-menu-item index="/strategy2">策略2</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-    </el-menu>
-  </el-aside>
-  
-  <el-container>
-    <el-header style="text-align: left; font-size: 16px">
-      <el-dropdown @command="handleCommand">
-        <i class="el-icon-more" style="margin-right: 20px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item command="logout">退出登录</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>{{ this.user.user }}</span>
-      <span>，欢迎使用 GooseGains 量化交易平台</span>
+      </el-menu-item-group>
       
-    </el-header>
-  
-  
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title"><i class="el-icon-setting"></i>量化交易</template>
+      
+        <el-menu-item index="/trade">买入卖出</el-menu-item>
+      
+      <el-submenu index="3-2">
+        <template slot="title">交易策略</template>
+        <el-menu-item index="/strategy1">策略1</el-menu-item>
+        <el-menu-item index="/strategy2">策略2</el-menu-item>
+      </el-submenu>
+    </el-submenu>
 
-    <el-main>
-      <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="地址">
-        </el-table-column>
-      </el-table>
-    </el-main>
-  </el-container>
+  </el-menu>
+</el-aside>
+
+<el-container>
+  <el-header style="text-align: left; font-size: 16px">
+    <el-dropdown @command="handleCommand">
+      <i class="el-icon-more" style="margin-right: 20px"></i>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item command="logout">退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <span>{{ this.user.user }}</span>
+    <span>，欢迎使用 GooseGains 量化交易平台</span>
+    
+  </el-header>
+
+
+
+  <el-main style="position: relative; background-image: url('https://cdn.pixabay.com/photo/2017/03/29/23/16/steel-2186813_1280.jpg'); background-size: cover; background-position: center; background-color: rgba(255, 255, 255, 0.6); padding: 20px;">
+    <div style="position: relative;">
+      <!-- 调整黑框位置 -->
+      <div style="position: absolute; top: 20px; left: 20px; width: calc(100% - 40px); padding: 20px; background-color: rgba(0, 0, 0, 0.5); border-radius: 10px;">
+        <!-- 添加标题 -->
+        <h1 style="color: white; font-size: 36px; margin-bottom: 20px;">关于 GooseGains</h1>
+        <!-- 添加文字内容 -->
+         
+        <div style="color: white; line-height: 1.8;">
+          <p style="font-size: 23px;font-weight: bold;">欢迎来到量化交易平台GooseGains！当前版本为1.0.</p>
+         
+          <p style="font-weight: bold;font-size: 20px;">我们的导航页包括了三个主要部分：</p>
+
+<div style="background-color: rgba(0, 0, 0, 0.5); padding: 10px;">
+    <p style="text-indent:20px;font-size: 18px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); color: white;">1.个人信息：在这里，您可以轻松访问和管理您的个人资料、账户设置和偏好设定。我们致力于为您提供最佳的个性化体验，确保您能够充分利用我们的平台功能。</p>
+</div>
+<div style="background-color: rgba(0, 0, 0, 0.5); padding: 10px;">
+<p style="text-indent:20px;font-size: 18px; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">2.市场数据：GooseGains提供最全面的市场数据服务。我们涵盖了上交所和深交所上千支股票的当日数据，以及上交所和深交所各15支股票的当日和历史数据。不论您是初学者还是专业人士，我们的市场数据都能满足您的需求，助您做出明智的投资决策。</p>
+</div>
+
+<div style="background-color: rgba(0, 0, 0, 0.5); padding: 10px;">
+<p style="text-indent:20px;font-size: 18px;">3.量化交易：在GooseGains，我们致力于提供最先进的量化交易工具和策略。我们的平台结合了最新的技术和深度分析，帮助您实现自动化交易执行，降低风险并最大化收益。无论您是寻求长期投资还是短期交易，我们都能为您提供量身定制的解决方案。</p>
+</div>
+<br>
+<p style="font-size: 18px;">在市场数据部分，我们采用了多种策略，以确保您获取到最准确、最及时的数据。我们的数据团队精心筛选和整理了上交所和深交所的大量股票数据，包括当日交易数据和历史数据。我们还提供了多种数据分析工具，帮助您更好地理解市场走势和趋势。
+
+无论您是新手投资者还是资深交易员，GooseGains都能为您提供所需的市场数据和量化交易工具，助您实现投资目标并取得成功！</p>
+      </div>
+    </div>
+  </div>
+  </el-main>
+
+
+
 </el-container>
+  </el-container>
 </template>
 
 <script>
 export default{
-  name:"Home2",
-  data() {
-    return {
-      user: {}, // 初始化为空对象
-      tableData: [] // 初始化为空数组
+name:"Home2",
+data() {
+  return {
+    user: {}, // 初始化为空对象
+    tableData: [] // 初始化为空数组
+  };
+  return {
+      platformName: 'GooseGains',
+      platformVersion: '1.0',
+      platformGuide: 'Goose',
+      platformFeatures: '个人资产情况 | 市场的历史+当日走势数据 | 金融产品买卖',
+      platformDescription: '你可以在这里介绍你的目标、使命、价值观，或者是你的产品或服务的特点和优势。',
     };
-  },
-  mounted() {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      // 如果存在存储的用户信息，则解析并更新 user 数据
-      this.user = JSON.parse(storedUser);
-      console.log("用户信息：", this.user)
-    }
-  },
-  methods: {
-      handleCommand(command) {
-        if (command==='logout'){
-          this.$router.push('/login');
-        }
-        if (command==='home'){
-          this.$router.push('/home')
-        }
-          
+},
+mounted() {
+  const storedUser = localStorage.getItem("user");
+  if (storedUser) {
+    // 如果存在存储的用户信息，则解析并更新 user 数据
+    this.user = JSON.parse(storedUser);
+    console.log("用户信息：", this.user)
+  }
+},
+methods: {
+    handleCommand(command) {
+      if (command==='logout'){
+        this.$router.push('/login');
       }
+      if (command==='home'){
+        this.$router.push('/home')
+      }
+        
     }
+  }
 }
 </script>
 <style>
 .el-header {
-  background-color: rgb(55, 128, 224,0.4);
+  background-color: #8694a7;
   color: #333;
   line-height: 60px;
 }
-  
+
 .el-aside {
-    color: #333;
+  color: #333;
 }
 </style>
